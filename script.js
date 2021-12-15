d5b6b20e6775cdf1304557a7af36d87a9b4ecacf
Elin Ahlgren <elin.ahlgren@hotmail.com>
2021-12-15 15:46:03 +0100

    hopefully done soon

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  // function showSearchInput() {
+  //   document.getElementById("searchInput").style.display = "block";
+  // }
+
+document.querySelector('#searchArea').addEventListener('click', e => {
+
+  if ( e.target === document.getElementById("searchInput") || document.getElementById("searchSpan") ) {
+    document.getElementById("searchInput").style.display = "block";
+  } else {
+
+  }
+
+})
+
+
+  /*
+function scaleUp() {
+  let a = document.getElementById("h1");
+  let b = document.getElementById("h2");
+  let c = document.getElementById("h3");
+
+  a.style.fontSize = "32px";
+  b.style.fontSize = "24px";
+  c.style.fontSize = "20px";
+
+}
+
+function scaleDown() {
+  let a = document.getElementById("h1");
+  let b = document.getElementById("h2");
+  let c = document.getElementById("h3");
+
+  a.style.fontSize = "30px";
+  b.style.fontSize = "22px";
+  c.style.fontSize = "18px";
+}
+*/
