d19eb3bde88fefd0d597ce3f7740912898688844
Elin Ahlgren <elin.ahlgren@hotmail.com>
2021-12-16 22:01:01 +0100

    finished

--- script.js
+++ script.js
@@ -7,39 +7,11 @@ function myFunction() {
     }
   }
 
-  // function showSearchInput() {
-  //   document.getElementById("searchInput").style.display = "block";
-  // }
-
-
-document.querySelector('#searchArea').addEventListener('click', e => {
-  if ( e.target === document.getElementById("searchInput") || document.getElementById("searchSpan") ) {
-    document.getElementById("searchInput").style.display = "block";
+function myFunctionSearch() {
+  var x = document.getElementById("searchBarDiv");
+  if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
-
+    x.style.display = "block";
   }
-})
-
-
-  /*
-function scaleUp() {
-  let a = document.getElementById("h1");
-  let b = document.getElementById("h2");
-  let c = document.getElementById("h3");
-
-  a.style.fontSize = "32px";
-  b.style.fontSize = "24px";
-  c.style.fontSize = "20px";
-
 }
-
-function scaleDown() {
-  let a = document.getElementById("h1");
-  let b = document.getElementById("h2");
-  let c = document.getElementById("h3");
-
-  a.style.fontSize = "30px";
-  b.style.fontSize = "22px";
-  c.style.fontSize = "18px";
-}
-*/
